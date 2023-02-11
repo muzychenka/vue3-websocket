000cf5b37b3b47513468ee957b99b7ad07aad156
Dmitry Muzychenko <nogrevonlight@gmail.com>
2023-02-11 09:25:11 +0300

    improvement: remove extra files

--- constants.ts
+++
@@ -1,5 +0,0 @@
-export const EVENT_OPEN: "open" = "open";
-export const EVENT_CLOSE: "close" = "close";
-export const EVENT_MESSAGE: "message" = "message";
-export const EVENT_ERROR: "error" = "error";
-export const DEFAULT_RECONNECT_TIME: number = 2000;
